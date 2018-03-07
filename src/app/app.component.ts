d2e9229e6309de84b5a7087867d73efa45a211fd
Mitesh <mitijain123@gmail.com>
2018-03-07 17:27:22 +0530

    added the change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map'
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+  	let header = new HttpHeaders();
+    header.append('Authorization',"Basic " + btoa("karaf:karaf") );
+
+  	this.http.get('https://localhost:9443/cxs/rules', { headers: header }).subscribe(data => {
+      console.log(data);
+    });
+	    	
+  }
+
 }
